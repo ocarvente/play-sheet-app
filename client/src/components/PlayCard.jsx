32fcd0cc3e15eb64575a946dfd0986152f25c2e5
Oscar Carvente <oscar.carvente@gmail.com>
2023-08-18 15:58:53 -0700

    Added delete icon and styled for it to be in top right corner of box

--- client/src/components/PlayCard.jsx
+++ client/src/components/PlayCard.jsx
@@ -2,16 +2,27 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import {Link} from 'react-router-dom';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import Box from '@mui/material/Box';
 const PlayCard = ({play}) => {
   return (
 
       <Grid item xs={4}>
         <Link to={`/play/${play.play_id}`}>
           <Paper>
-            <Typography>{play.play_name}</Typography>
+            <Box sx={{display: 'flex', flexDirection: 'row' , alignItems: 'center',  justifyContent:'space-between',}}>
+              <Typography>{play.play_name}</Typography>
+              <Tooltip title="Delete">
+                <IconButton>
+                <DeleteIcon />
+                </IconButton>
+              </Tooltip>
+            </Box>
             <img src ={play.play_url_photo} width={250}/>
-          </Paper>
 
+          </Paper>
         </Link>
       </Grid>
 

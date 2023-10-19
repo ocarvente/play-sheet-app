c1dfe07fa86f4ea05d81cc170e3dcfdbac8ca1cc
Oscar Carvente <oscar.carvente@gmail.com>
2023-10-18 17:42:16 -0700

    Updated sql schema to include categories and categorized plays table

--- server/database/postgresSchema.sql
+++ server/database/postgresSchema.sql
@@ -2,8 +2,7 @@ DROP DATABASE IF EXISTS football;
 
 CREATE DATABASE football;
 
-\c football
-DROP TABLE IF EXISTS plays;
+\c football;
 
 CREATE TABLE plays (
   play_id SERIAL PRIMARY KEY,
@@ -12,11 +11,22 @@ CREATE TABLE plays (
   play_description TEXT NOT NULL
 );
 
+CREATE TABLE categories (
+  category_id SERIAL PRIMARY KEY,
+  category_name VARCHAR(200) NOT NULL
+);
 
-------COPY preset data -----
+CREATE TABLE categorizedPlays (
+  id SERIAL PRIMARY KEY,
+  play_id INTEGER references plays(play_id) ON DELETE CASCADE,
+  category_id INTEGER references categories(category_id) ON DELETE CASCADE
+);
 
--- COPY plays (play_id, play_name, play_url_photo, play_description)
--- FROM '/Users/oscarcarvente/Desktop/plays.csv'
--- DELIMITER ',' CSV HEADER;
+CREATE INDEX idx_play_id ON categorizedPlays (play_id);
+CREATE INDEX idx_category_id ON categorizedPlays (category_id);
+
+COPY plays (play_id, play_name, play_url_photo, play_description)
+FROM '/Users/oscarcarvente/Desktop/plays.csv'
+DELIMITER ',' CSV HEADER;
 
 

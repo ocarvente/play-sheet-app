d41ab7bbcfe35ab70d7b35294f0f94f3f5c87084
Oscar Carvente <oscar.carvente@gmail.com>
2024-01-16 21:16:21 -0800

    Production server code removed static file serving

--- server/database/index.js
+++ server/database/index.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const {Pool} = require('pg');
 
 const pool = new Pool({
-  // connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-  host:'localhost',
-  user:'oscarcarvente',
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  // host:'localhost',
+  // user:'oscarcarvente',
+  // max: 20,
+  // idleTimeoutMillis: 30000,
+  // connectionTimeoutMillis: 2000
 
 })
 

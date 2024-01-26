384093cf59ecf7a032184ec20db545242abb2f40
Oscar Carvente <oscar.carvente@gmail.com>
2024-01-25 16:29:22 -0800

    choses random play correctly without reloading page

--- server/index.js
+++ server/index.js
@@ -12,9 +12,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
-app.get('/', (req,res) => {
-  res.send('hello world');
-})
+app.use('/', express.static(path.join(__dirname, '../client/dist')))
+
 app.get('/plays',async (req, res) => {
   try {
     const data = req.body;

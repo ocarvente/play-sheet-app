e1a7ed2b5a1e65b360d3773d636bb41d8b1d3d4b
Oscar Carvente <oscar.carvente@gmail.com>
2024-06-02 21:44:15 -0700

    creates endpoint to check if user exists in database

--- server/index.js
+++ server/index.js
@@ -1,92 +1,105 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const{getAllPlays, getPlayRandom, getPlay, createPlay, deletePlay, updatePlay} = require('./models');
-const path = require('path');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const {
+  getAllPlays,
+  getPlayRandom,
+  getPlay,
+  createPlay,
+  deletePlay,
+  updatePlay,
+  checkUser,
+} = require("./models");
+const path = require("path");
 const app = express();
-var cors = require('cors');
+var cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/', express.static(path.join(__dirname, '../client/dist')))
+app.use("/", express.static(path.join(__dirname, "../client/dist")));
 
-app.get('/plays',async (req, res) => {
+app.get("/plays", async (req, res) => {
   try {
     const data = req.body;
     const plays = await getAllPlays(data);
-    res.send(plays)
-  } catch(error){
+    res.send(plays);
+  } catch (error) {
     res.status(404).send();
   }
-})
+});
 
-app.post('/plays', async(req, res) => {
+app.post("/plays", async (req, res) => {
   try {
     const result = await createPlay(req.body);
     res.send(result);
-  } catch(error) {
+  } catch (error) {
     res.status(404);
     res.send(error);
   }
-
 });
 
-app.get('/play/random', async(req, res) => {
+app.get("/play/random", async (req, res) => {
   try {
     const total = await getPlayRandom();
     res.send(total);
-  } catch(error) {
+  } catch (error) {
     res.status(404).send();
   }
-})
+});
 
-app.get('/plays/:id', async(req, res) => {
+app.get("/plays/:id", async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const play = await getPlay(id);
     res.send(play);
-  } catch(error) {
+  } catch (error) {
     res.status(404).send();
   }
-})
-app.delete('/plays/:id', async (req, res) => {
+});
+app.delete("/plays/:id", async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const result = await deletePlay(id);
     res.send(result);
-  } catch(error) {
+  } catch (error) {
     res.status(404).send();
   }
-})
+});
 
-app.patch('/plays/:id', async (req, res) => {
+app.patch("/plays/:id", async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const result = await updatePlay(id, req.body);
     res.send(result);
-  } catch(error) {
+  } catch (error) {
     res.status(404).send();
   }
-})
+});
 
-
-app.get('*', (req,res) =>{
-  res.sendFile(path.resolve(__dirname, '../client/dist/index.html'));
+app.post("/check-auth", async (req, res) => {
+  try {
+    const { email } = req.body;
+    console.log(req.body);
+    const user = await checkUser(email);
+    console.log(user);
+    res.send({
+      status: user.length === 1 ? "User exists" : "User does not exist",
+      userExists: user.length === 1,
+    });
+  } catch (e) {}
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
 });
 
 app.listen(PORT, (err) => {
-  if(err) {
-    console.log('there was an error connecting');
+  if (err) {
+    console.log("there was an error connecting");
   } else {
-    console.log("connection successful")
+    console.log(`connection successful on ${PORT}`);
   }
-
 });
-
-
-
-
--- server/models/index.js
+++ server/models/index.js
@@ -1,65 +1,78 @@
-const db = require('../database');
+const db = require("../database");
 module.exports = {
-  getAllPlays:  async ()=> {
-    const queryString = 'SELECT * FROM plays ORDER BY play_id'
+  getAllPlays: async () => {
+    const queryString = "SELECT * FROM plays ORDER BY play_id";
     try {
-     const plays = await db.query(queryString);
-     return plays.rows;
-    } catch(error) {
-      console.log('query didnt go through, ', error);
+      const plays = await db.query(queryString);
+      return plays.rows;
+    } catch (error) {
+      console.log("query didnt go through, ", error);
     }
   },
 
   getPlayRandom: async () => {
     try {
-      const queryString = 'SELECT * FROM plays ORDER BY RANDOM() LIMIT 1';
+      const queryString = "SELECT * FROM plays ORDER BY RANDOM() LIMIT 1";
       const total = await db.query(queryString);
       return total.rows;
-    } catch(error) {
-      console.log('unable to get total, ', error);
+    } catch (error) {
+      console.log("unable to get total, ", error);
     }
   },
 
   getPlay: async (id) => {
-    const queryString = 'SELECT * FROM plays WHERE play_id = $1'
+    const queryString = "SELECT * FROM plays WHERE play_id = $1";
     const values = [id];
     try {
       const play = await db.query(queryString, values);
       return play.rows;
-    }catch(error) {
-      console.log('unable to get play, ', error);
+    } catch (error) {
+      console.log("unable to get play, ", error);
     }
   },
-  createPlay: async(data) => {
-    const {play_name, play_url_photo, play_description} = data;
-    const queryString = 'INSERT INTO plays (play_name, play_url_photo, play_description) VALUES ($1, $2, $3)'
+  createPlay: async (data) => {
+    const { play_name, play_url_photo, play_description } = data;
+    const queryString =
+      "INSERT INTO plays (play_name, play_url_photo, play_description) VALUES ($1, $2, $3)";
     const values = [play_name, play_url_photo, play_description];
     try {
       const queryResult = await db.query(queryString, values);
       return queryResult;
-    } catch(error) {
-      console.log('unable to post play, ', error);
+    } catch (error) {
+      console.log("unable to post play, ", error);
     }
   },
   deletePlay: async (id) => {
-    const queryString = 'DELETE FROM plays WHERE play_id = $1';
+    const queryString = "DELETE FROM plays WHERE play_id = $1";
     const values = [id];
     try {
       const queryResult = await db.query(queryString, values);
       return queryResult;
-    } catch(error) {
-      console.log('unable to delete play, ', error);
+    } catch (error) {
+      console.log("unable to delete play, ", error);
     }
   },
   updatePlay: async (id, data) => {
     try {
-      const {play_name, play_url_photo, play_description} = data;
-      const queryString = 'UPDATE plays SET play_name = $1,play_url_photo = $2, play_description = $3 WHERE play_id = $4';
+      const { play_name, play_url_photo, play_description } = data;
+      const queryString =
+        "UPDATE plays SET play_name = $1,play_url_photo = $2, play_description = $3 WHERE play_id = $4";
       const values = [play_name, play_url_photo, play_description, id];
       const queryResult = await db.query(queryString, values);
       return queryResult;
-    } catch(error) {
-      console.log('unable to update play, ', error);
+    } catch (error) {
+      console.log("unable to update play, ", error);
     }
-  }
-}
+  },
+
+  checkUser: async (email) => {
+    try {
+      console.log(email);
+      const queryString = "SELECT * FROM users WHERE email = $1";
+      const user = await db.query(queryString, [email]);
+      return user.rows;
+    } catch (e) {
+      console.error("unable to execute finding email query, ", error);
+    }
+  },
+};
